b35189b5e08cb9d27a07b605214d39d6e397a73f
Dung Nguyen <dung.nguyenthimy@nashtechglobal.com>
2024-10-21 15:39:52 +0700

    test

--- obj/Debug/net8.0/GameStore.api.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+742ed8102ecff2dc627eece127ffe0951bc67dbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
